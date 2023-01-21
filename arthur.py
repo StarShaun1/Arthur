196266576f784941551bc26fd76f75c0f11a5464
star_shaun <69088477+StarShaun1@users.noreply.github.com>
2023-01-21 13:48:02 -0400

    Add files via upload

---
+++ arthur.py
@@ -0,0 +1,17 @@
+print("                 #                                      ")
+print("                # #   #####  ##### #    # #    # #####  ")
+print("               #   #  #    #   #   #    # #    # #    # ")
+print("              #     # #    #   #   ###### #    # #    # ")
+print("              ####### #####    #   #    # #    # #####  ")
+print("              #     # #   #    #   #    # #    # #   #  ")
+print("              #     # #    #   #   #    #  ####  #    # ")
+
+lol = input("squeeze your left nut and beg for mercy (y/n) : ")
+
+if lol == "y":
+	print("i spare you")
+else:
+	if lol == "n":
+		print("you die")
+else:
+	print("bitch u didnt even say either of those")
